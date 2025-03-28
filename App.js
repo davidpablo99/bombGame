b78d79d5f0b2f782bfe715e89bbe3f705baefcd0
David Pablo Cardoso Breda Queiroz <davidpablo99@gmail.com>
2025-03-27 21:35:18 -0400

    Finalizando a pagina de regras

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import Start from './src/pages/Start';
+import Rules from './src/pages/Rules';
+import { StatusBar } from 'react-native';
 
 export default function App() {
-  return <Start/>
+  return (
+  <>
+    <StatusBar color="light"/>
+    <Rules/>
+  </>
+);
 }
